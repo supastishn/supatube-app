ac37bdc644051c4ca480d9631a03c2c0e7456248
supastishn <omareeto2012@hotmail.com>
2025-08-09 17:05:51 +0000

    fix: remove resolver blockList to allow module resolution in metro config

--- app/metro.config.js
+++ app/metro.config.js
@@ -5,22 +5,7 @@ const path = require('path');
 // Get default config for this app directory
 const config = getDefaultConfig(__dirname);
 
-// Normalize blockList: handle both arrays and single values
-const currentBlockList = config.resolver.blockList || [];
-const normalizedBlockList = Array.isArray(currentBlockList) 
-  ? currentBlockList 
-  : [currentBlockList].filter(Boolean);
-
-// Optimize watch performance by:
-// 1. Only watching our app directory
-// 2. Ignoring node_modules recursively
+// Optimize watch performance by watching only our app directory
 config.watchFolders = [path.resolve(__dirname)];
-config.resolver = {
-  ...config.resolver,
-  blockList: [
-    ...normalizedBlockList,
-    /.*\/node_modules\/.*/,
-  ]
-};
 
 module.exports = config;
