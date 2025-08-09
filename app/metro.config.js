e155e06171baf2fd6650dcee0c9898f851d5bcc2
supastishn <omareeto2012@hotmail.com>
2025-08-09 20:21:30 +0000

    fix: remove custom watchFolders from metro config to prevent ENOSPC error

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,11 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 // Get default config for this app directory
 const config = getDefaultConfig(__dirname);
-
-// Optimize watch performance by watching only our app directory
-config.watchFolders = [path.resolve(__dirname)];
 
 module.exports = config;
