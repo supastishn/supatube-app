123901649f99afd24a22fd230aad1ed70047c3ef
supastishn <omareeto2012@hotmail.com>
2025-08-09 16:52:23 +0000

    fix: update metro config to ignore node_modules in file watchers

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,7 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-/** @type {import('expo/metro-config').MetroConfig} */
+// Get default config for this app directory
 const config = getDefaultConfig(__dirname);
+
+// Optimize watch performance by:
+// 1. Only watching our app directory
+// 2. Ignoring node_modules recursively
+config.watchFolders = [path.resolve(__dirname)];
+config.resolver = {
+  ...config.resolver,
+  blockList: [
+    ...(config.resolver.blockList || []),
+    /.*\/node_modules\/.*/,
+  ]
+};
 
 module.exports = config;
