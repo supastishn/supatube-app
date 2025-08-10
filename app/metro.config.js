c859f8630e00e68ba69c51fbd41e6cf36cb51b86
supastishn <omareeto2012@hotmail.com>
2025-08-10 08:46:13 +0000

    fix: exclude unnecessary node_modules dirs from Metro watcher to prevent ENOSPC

--- app/metro.config.js
+++ app/metro.config.js
@@ -4,4 +4,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 // Get default config for this app directory
 const config = getDefaultConfig(__dirname);
 
+// To prevent ENOSPC errors (too many files watched), we can tell Metro
+// to not watch certain directories within node_modules.
+config.resolver.blockList = [
+  /node_modules\/.*\/__tests__\/.*/,
+  /node_modules\/.*\/android\/.*/,
+  /node_modules\/.*\/ios\/.*/,
+  /node_modules\/.*\/\\.git\/.*/,
+  /node_modules\/.*\/\\.github\/.*/,
+  /node_modules\/.*\/\\.vscode\/.*/,
+];
+
 module.exports = config;
