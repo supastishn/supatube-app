8bc834d8c871d1790bcd37c599200b2098d07dd0
supastishn <omareeto2012@hotmail.com>
2025-08-10 12:55:43 +0000

    fix: update Metro blockList to only ignore root node_modules and backend dirs

--- app/metro.config.js
+++ app/metro.config.js
@@ -10,10 +10,9 @@ const projectRoot = path.resolve(__dirname, '..');
 // To prevent ENOSPC errors (too many files watched), we can tell Metro
 // to not watch certain directories.
 config.resolver.blockList = [
-  // Globally ignore all node_modules directories. This is a broad but effective
-  // way to prevent ENOSPC errors from too many files being watched.
-  /.*\/node_modules\/.*/,
-  // Ignore the backend directory
+  // Ignore the root node_modules directory.
+  new RegExp(`${projectRoot}/node_modules/.*`),
+  // Ignore the backend directory.
   new RegExp(`${projectRoot}/backend/.*`),
 ];
 
